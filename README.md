f8e523369f5e73639640e9bdf5209138048604d4
Suraj Variar <sooraj.varrier@gmail.com>
2022-02-05 17:19:09 +0530

    created a new react app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react
+
+>> npm i create-react-app -g
+
+>>create-react-app react-app  //command to create a react app
+
+>>npm start //to start the application
---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//state component
+class App extends React.Component{
+  render(){
+    return <h1>Hello React User</h1>
+  }
+}
+
+export default App
