0112c1a879aba6598d4d8cd01522659555d6f7d9
Suraj Variar <sooraj.varrier@gmail.com>
2022-02-06 12:42:03 +0530

    added state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 
 
 //state component
 class App extends React.Component{
-  render(){
-    return <h1>Hello React User</h1>
+  constructor(){
+    super();
+    this.state={
+      txt:'This is the initial state'
+    }
   }
+  update (e){
+    this.setState({
+      txt:e.target.value
+    })
+  }
+  render(){
+    return <div>
+      <h1>{this.state.txt}</h1>
+      <input type="text" onChange={this.update.bind(this)}></input>&nbsp;
+      <Button>React</Button>
+      </div>
+  }
+}
+
+const Button=(props)=><button>{props.children}</button>
+
+App.propTypes={
+  txt:PropTypes.string
+}
+
+App.defaultProps={
+  txt:"This is a default value"
 }
 
 export default App
